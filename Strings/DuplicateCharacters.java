2bbb87129066d65ad21b4c87faa3f640b1d307ad
Suhani Shinde <suhanishinde551@gmail.com>
2026-02-20 14:57:58 +0530

    Day 10-Duplicate Characters in String

---
+++ Strings/DuplicateCharacters.java
@@ -0,0 +1,29 @@
+package Strings;
+import java.util.Scanner;
+
+public class DuplicateCharacters {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine().toLowerCase();
+
+        int[] freq = new int[256];
+
+        for(int i = 0; i < str.length(); i++) {
+            freq[str.charAt(i)]++;
+        }
+
+        System.out.println("Duplicate characters:");
+
+        for(int i = 0; i < 256; i++) {
+            if(freq[i] > 1) {
+                System.out.println((char)i);
+            }
+        }
+
+        sc.close();
+    }
+}
