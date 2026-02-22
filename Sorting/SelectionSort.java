dbe174d170e28ce60de7aff65bbd6799d6bd820e
Suhani Shinde <suhanishinde551@gmail.com>
2026-02-22 22:28:44 +0530

    Day 11-Selection Sorting

---
+++ Sorting/SelectionSort.java
@@ -0,0 +1,47 @@
+    package Sorting;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Selection Sort
+        for(int i = 0; i < n - 1; i++) {
+
+            int minIndex = i;
+
+            for(int j = i + 1; j < n; j++) {
+                if(arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println("Sorted array:");
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
+
