e68cf6bfcb015e6a449d51a2a8c34cd144f71ccc
Suhani Shinde <suhanishinde551@gmail.com>
2026-02-25 22:46:44 +0530

    Day 15- Factorial and Sum using Recursion

---
+++ Recursion/FactorialRecursion.java
@@ -0,0 +1,28 @@
+    package Recursion;
+
+import java.util.Scanner;
+
+public class FactorialRecursion {
+
+    public static int factorial(int n) {
+        if(n == 0 || n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = factorial(n);
+
+        System.out.println("Factorial: " + result);
+
+        sc.close();
+    }
+}
+
