5f51bdb89442e892d7fa01f39fd55465e4355345
Suhani Shinde <suhanishinde551@gmail.com>
2026-03-05 22:02:44 +0530

    Day 21 - Fibonacci and reverse print using recursion

---
+++ Recursion/FibonacciRecursion.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class FibonacciRecursion {
+
+    public static int fibonacci(int n) {
+
+        if(n == 0)
+            return 0;
+
+        if(n == 1)
+            return 1;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = fibonacci(n);
+
+        System.out.println("Fibonacci number: " + result);
+
+        sc.close();
+    }
+}
