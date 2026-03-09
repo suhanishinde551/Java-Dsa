19a18ba3a6cc159cf7941f7cfe01b375bd7bc761
Suhani Shinde <suhanishinde551@gmail.com>
2026-03-09 23:52:28 +0530

    Day 24 - Find missing number in array

---
+++ Arrays/FindMissingNumber.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,4,5};
+
+        int n = arr.length + 1;
+
+        int expectedSum = n * (n + 1) / 2;
+
+        int actualSum = 0;
+
+        for(int i = 0; i < arr.length; i++) {
+            actualSum += arr[i];
+        }
+
+        int missing = expectedSum - actualSum;
+
+        System.out.println("Missing number: " + missing);
+    }
+}
