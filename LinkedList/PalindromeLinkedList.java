f2b926d8b3f6ffa9325e90ebc91210e591f35e40
Suhani Shinde <suhanishinde551@gmail.com>
2026-03-24 22:38:12 +0530

    Day - 30 Checked palindrome in linked list using reverse method

---
+++ LinkedList/PalindromeLinkedList.java
@@ -0,0 +1,112 @@
+package LinkedList;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class PalindromeLinkedList
+ {
+
+    Node head;
+
+    // Insert at end
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Display
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Find middle
+    public Node findMiddle(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // Reverse list
+    public Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+
+    // Check palindrome
+    public boolean isPalindrome() {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node mid = findMiddle(head);
+
+        Node secondHalf = reverse(mid);
+
+        Node firstHalf = head;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data)
+                return false;
+
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        PalindromeLinkedList list = new PalindromeLinkedList();
+
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(2);
+        list.insert(1);
+
+        list.display();
+
+        if (list.isPalindrome()) {
+            System.out.println("Palindrome Linked List");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+    }
+}
